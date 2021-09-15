929f0a0c9d8c3ea8850a689d170e45d98f963cbc
Alex <suhobski@mail.ru>
2021-09-15 15:15:46 +0300

    fix: add material-ui package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import {makeStyles, Paper} from "@material-ui/core"
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
-  );
+const useStyles = makeStyles(theme => ({
+    root: {
+        background: 'red',
+        // position: 'relative',
+        // display: 'flex',
+        // alignItems: 'flex-start',
+        // [theme.breakpoints.down('sm')]: {
+        //     flexDirection: 'column',
+        //     justifyItems: 'stretch',
+        // }
+    },
+  }))
+
+const App = () => {
+    const classes = useStyles();
+
+    return (
+        <Paper className={classes.root}>
+            <header>
+                <h1>Hello!</h1>
+            </header>
+            <p>Article</p>
+        </Paper>
+    );
 }
 
 export default App;
