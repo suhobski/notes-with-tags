390c7daf4d79cce46f69ace02916d6327a9d5ca2
Alex <suhobski@mail.ru>
2021-09-17 22:47:24 +0300

    feat: add Header

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {makeStyles} from "@material-ui/core"
+
+const useStyles = makeStyles({
+    Header: {
+        padding: 8,
+        color: '#5A5A65',
+        background: '#FFFFFF',
+        borderRadius: 12,
+
+    }
+})
+
+const Header = () => {
+
+    const classes = useStyles();
+
+    return (
+        <header className={classes.Header}>
+            <h1>Notes with tags</h1>
+        </header>    
+    );
+}
+
+export default Header;
