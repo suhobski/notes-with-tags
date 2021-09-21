4830e401bbb9d715e9e2a3ff5bdf0571bdfc74f0
Alex <suhobski@mail.ru>
2021-09-21 15:56:13 +0300

    refactor: edit Note styles

--- src/components/Note.js
+++ src/components/Note.js
@@ -4,23 +4,26 @@ import {Card, makeStyles} from "@material-ui/core"
 const useStyles = makeStyles({
     note: {
         marginBottom: 8,
+        padding: 8,
+        paddingBottom: 0,
+        color: '#5A5A65',
         '&:last-child': {
             marginBottom: 0,
-        },
-        padding: 8,
-        '&__text': {
-
-        },
-        
+        },        
+    },
+    note__text: {
+        padding: '8px 0',
     },
     'note__date': {
-        
+        fontSize: 12,
+        borderRadius: 4,
     },
     'note__tag-list': {
         margin: 0,
     },
     'note__tag': {
         display: 'inline-block',
+        margin: '0 8px 8px 0',
         padding: '0 4px',
         listStyleType: 'none',
         borderRadius: 4,
@@ -29,16 +32,19 @@ const useStyles = makeStyles({
     }
 })
 
-const Note = () => {
-
+const Note = ({note}) => {
     const classes = useStyles();
+    const {noteDate, noteText, noteTags} = note;
+    const date = noteDate.toLocaleTimeString().substring(0, 5) + ' ' + noteDate.toLocaleDateString();
 
     return (
         <Card className={classes.note}>
-            <p className={classes.note__date}>15.09.2021 22:32</p>
-            <p className={classes.note__text}>I want go to the #shop</p>
+            <p className={classes.note__date}>{date}</p>
+            <p className={classes.note__text}>{noteText}</p>
             <ul className={classes['note__tag-list']}>
-                <li className={classes.note__tag}>#shop</li>
+            {
+                noteTags && noteTags.map((tag, index) => <li className={classes.note__tag} key={tag + index}>{tag}</li>)
+            }
             </ul>
         </Card>    
     );
