d3f1e7e260a41ba48f4502196a72c1d89c1936d7
Alex <suhobski@mail.ru>
2021-09-22 23:04:13 +0300

    feat: add button edit and delete note

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,6 +3,7 @@ import {Card, makeStyles} from "@material-ui/core"
 
 const useStyles = makeStyles({
     note: {
+        position: 'relative',
         marginBottom: 8,
         padding: 8,
         paddingBottom: 0,
@@ -29,6 +30,40 @@ const useStyles = makeStyles({
         borderRadius: 4,
         background: '#5A5A65',
         color: '#FFFFFF',
+    },
+    'note__button--edit': {
+        position: 'absolute',
+        top: 6,
+        right: 40,
+        width: 24,
+        height: 24,
+        outline: 'none',
+        borderRadius: 32,
+        border: 'none',
+        background: 'url(./img/pen.png) no-repeat center',
+        backgroundSize: '17px',
+        cursor: 'pointer',
+        transition: 'all .3s ease-out',
+        '&:active': {
+            backgroundColor: '#e0e0e0',
+        }
+    },
+    'note__button--delete': {
+        position: 'absolute',
+        top: 6,
+        right: 8,
+        width: 24,
+        height: 24,
+        outline: 'none',
+        borderRadius: 32,
+        border: 'none',
+        background: 'url(./img/trash.png) no-repeat center',
+        backgroundSize: '17px',
+        cursor: 'pointer',
+        transition: 'all .3s ease-out',
+        '&:active': {
+            backgroundColor: '#e0e0e0',
+        }
     }
 })
 
@@ -39,6 +74,8 @@ const Note = ({note}) => {
 
     return (
         <Card className={classes.note}>
+            <button className={classes['note__button--delete']} />
+            <button className={classes['note__button--edit']} />
             <p className={classes.note__date}>{date}</p>
             <p className={classes.note__text}>{noteText}</p>
             <ul className={classes['note__tag-list']}>
