477709c381ba33d8fcc52f5ecf436cb338e630be
Alex <suhobski@mail.ru>
2021-09-28 13:40:55 +0300

    refactor: change styles to scss

--- src/containers/ModalEditNote/ModalEditNote.js
+++ src/containers/ModalEditNote/ModalEditNote.js
@@ -1,100 +1,10 @@
 import React, { useState } from "react";
-import { Button, Card, makeStyles, TextField } from "@material-ui/core";
+import { Button, Card, TextField } from "@material-ui/core";
 import { connect } from "react-redux";
 import { styled } from "@mui/material/styles";
 import { Box } from "@mui/system";
-import { editNote } from "../store/actions/board";
-import "./ModalEditNote.scss";
-
-const useStyles = makeStyles({
-  modalEditNote: {
-    position: "fixed",
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    display: "grid",
-    gridTemplateColumns: "minmax(280px, 400px)",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 8,
-    background: "rgba(0, 0, 0, 0.5)",
-    zIndex: 40,
-  },
-  note: {
-    marginBottom: 8,
-    padding: 8,
-    paddingBottom: 0,
-    color: "#5A5A65",
-    "& ul": {
-      display: "inline-block",
-      width: "100%",
-      margin: "0 8px 0 0",
-      padding: 8,
-      paddingBottom: 0,
-      listStyleType: "none",
-      borderRadius: 4,
-      background: "#F8F8F8",
-      border: "none",
-      color: "#FFFFFF",
-    },
-  },
-  note__title: {
-    marginBottom: 8,
-  },
-  note__text: {
-    marginBottom: 8,
-    padding: "8px 16px",
-    borderRadius: 4,
-    background: "#F8F8F8",
-  },
-  note__date: {
-    fontSize: 12,
-    borderRadius: 4,
-    marginBottom: 8,
-  },
-  "note__tag-list": {
-    margin: 0,
-    padding: 8,
-    minHeight: 40,
-    borderRadius: 4,
-    background: "#F8F8F8",
-  },
-  note__tag: {
-    position: "relative",
-    display: "inline-block",
-    margin: "0 16px 8px 0",
-    padding: "0 4px",
-    listStyleType: "none",
-    borderRadius: 4,
-    background: "#5A5A65",
-    color: "#FFFFFF",
-  },
-  "note__tag--addTag": {
-    display: "inline-block",
-    height: 56,
-    margin: "8px 0 8px 8px",
-    padding: "0 4px",
-    listStyleType: "none",
-    borderRadius: 4,
-    background: "#5BD497",
-    border: "none",
-    color: "#FFFFFF",
-  },
-  tagInputWrapper: {
-    display: "grid",
-    gridTemplateColumns: "auto 100px",
-  },
-  footer: {
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr",
-    gap: 8,
-    margin: "8px 0",
-    "& Button": {
-      display: "inline-block",
-    },
-  },
-});
+import { editNote } from "../../store/actions/board";
+import styles from "./ModalEditNote.module.scss";
 
 const CssTextField = styled(TextField)({
   "&": {
@@ -110,8 +20,13 @@ const CssTextField = styled(TextField)({
   },
 });
 
+const ButtonAddTag = styled(Button)({
+  "&:hover": {
+    background: "#57cf92",
+  },
+});
+
 const ModalEditNote = ({ closeModal, notes, note, onEditNote }) => {
-  const classes = useStyles();
   const { noteDate, noteId, noteTags, noteText } = notes.find(
     (stateNote) => stateNote.noteId === note.noteId
   );
@@ -159,18 +74,18 @@ const ModalEditNote = ({ closeModal, notes, note, onEditNote }) => {
 
   return (
     <Box
-      className={classes.modalEditNote}
+      className={styles.modalEditNote}
       onSubmit={handleSubmit}
       component="form"
       noValidate
     >
-      <Card className={classes.note}>
-        <h2 className={classes.note__title}>Edit note</h2>
-        <p className={classes.note__date}>{date}</p>
+      <Card className={styles.note}>
+        <h2 className={styles.note__title}>Edit note</h2>
+        <p className={styles.note__date}>{date}</p>
         <article>
           <h4>Text:</h4>
           <CssTextField
-            className={classes.modalEditNote__textInput}
+            className={styles.modalEditNote__textInput}
             id="editNote"
             name="editNote"
             variant="outlined"
@@ -183,13 +98,13 @@ const ModalEditNote = ({ closeModal, notes, note, onEditNote }) => {
         </article>
         <article>
           <h4>Tags:</h4>
-          <ul className={classes["note__tag-list"]}>
+          <ul className={styles["note__tag-list"]}>
             {editNoteTags.length > 0 ? (
               editNoteTags.map((tag, index) => (
-                <li className={classes.note__tag} key={tag + index}>
+                <li className={styles.note__tag} key={tag + index}>
                   {tag}
                   <button
-                    className="note__tag--delete"
+                    className={styles["note__tag--delete"]}
                     onClick={() => deleteTag(tag)}
                   />
                 </li>
@@ -198,26 +113,26 @@ const ModalEditNote = ({ closeModal, notes, note, onEditNote }) => {
               <p>...</p>
             )}
           </ul>
-          <div className={classes.tagInputWrapper}>
+          <div className={styles.tagInputWrapper}>
             <CssTextField
               value={addTag}
               onChange={handleChangeTag}
-              className={classes.modalEditNote__textInput}
+              className={styles.modalEditNote__textInput}
               name="addTag"
               variant="outlined"
               label="Write a tag..."
             />
-            <Button
+            <ButtonAddTag
+              className={styles["note__tag--addTag"]}
               onClick={onAddTagClick}
               variant="contained"
-              className={classes["note__tag--addTag"]}
               key="AddTag"
             >
               Add tag
-            </Button>
+            </ButtonAddTag>
           </div>
         </article>
-        <footer className={classes.footer}>
+        <footer className={styles.footer}>
           <Button type="submit" variant="contained" fullWidth>
             Ok
           </Button>
