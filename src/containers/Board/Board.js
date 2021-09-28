a1dda8509a70c49d86024fc6d9b8ee504c0e3518
Alex <suhobski@mail.ru>
2021-09-28 14:40:16 +0300

    refactor: move Board's styles to scss

--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -1,58 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card, makeStyles } from "@material-ui/core";
+import { Button, Card } from "@material-ui/core";
 import { connect } from "react-redux";
-import Filter from "./Filter";
-import Note from "./Note";
-import CreateNoteForm from "./CreateNoteForm";
-
-const useStyles = makeStyles((theme) => ({
-  board: {
-    position: "relative",
-    padding: "16px 8px",
-    background: "#FFFFFF",
-    borderRadius: 12,
-    color: "#5A5A65",
-    "& > Button": {
-      position: "fixed",
-      bottom: 16,
-      right: 16,
-      padding: 6,
-      width: 40,
-      textAlign: "right",
-      fontSize: 28,
-      borderRadius: 100,
-      zIndex: 30,
-      transition: "all .3s ease-out",
-      [theme.breakpoints.up("sm")]: {
-        display: "none",
-      },
-    },
-  },
-  board__title: {
-    marginBottom: 8,
-  },
-  notesWrapper: {
-    padding: 8,
-    background: "#F8F8F8",
-    color: "#5A5A65",
-  },
-  ModalCreateNote: {
-    position: "fixed",
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    background: "rgba(0, 0, 0, 0.5)",
-    display: "grid",
-    gridTemplateColumns: "minmax(280px, 400px)",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "0 16px",
-  },
-}));
+import Filter from "../Filter/Filter";
+import Note from "../Note";
+import CreateNoteForm from "../CreateNoteForm";
+import styles from "./Board.module.scss";
 
 const Board = ({ filterTag, notes }) => {
-  const classes = useStyles();
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [buttonRotate, setButtonRotate] = useState(0);
   const [boardNotes, setBoardNotes] = useState(notes);
@@ -84,10 +38,10 @@ const Board = ({ filterTag, notes }) => {
   }, [notes, filterTag]);
 
   return (
-    <Card className={classes.board}>
-      <h2 className={classes.board__title}>Board</h2>
+    <Card className={styles.board} style={{ borderRadius: 12 }}>
+      <h2 className={styles.board__title}>Board</h2>
       <Filter />
-      <Card className={classes.notesWrapper}>
+      <Card className={styles.notesWrapper}>
         <h3>Notes</h3>
         {boardNotes.length === 0 ? (
           <p>...</p>
@@ -98,7 +52,7 @@ const Board = ({ filterTag, notes }) => {
         )}
       </Card>
       <Button
-        className={classes.button}
+        className={styles.button}
         variant="contained"
         onClick={handleButton}
         style={{ transform: `rotate(${buttonRotate})` }}
@@ -106,7 +60,7 @@ const Board = ({ filterTag, notes }) => {
         +
       </Button>
       {isOpenModal ? (
-        <Card className={classes.ModalCreateNote} onClick={handleModalClick}>
+        <Card className={styles.ModalCreateNote} onClick={handleModalClick}>
           <CreateNoteForm closeModal={() => closeModal()} />
         </Card>
       ) : null}
