16993b94e29a530f5866d38272f934cbccefb610
Alex <suhobski@mail.ru>
2021-10-02 23:57:11 +0300

    refactor: change App styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,52 @@
 import React from 'react';
-import { CardContent, makeStyles, Paper } from '@material-ui/core';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { CardContent, Container, styled } from '@material-ui/core';
 import Header from './components/Header/Header';
 import CreateNoteForm from './containers/CreateNoteForm/CreateNoteForm';
 import Board from './containers/Board/Board';
 import Footer from './components/Footer/Footer';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    position: 'relative',
-    display: 'grid',
-    gridTemplateRows: 'auto 1fr auto',
-    minHeight: '100vh',
-    padding: 8,
-    borderRadius: 0,
-    background: '#4D4D4D',
-    fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
-    zIndex: 0,
-  },
-  main: {
-    display: 'grid',
-    gridTemplateColumns: 'minmax(300px, 33%) auto',
-    gap: 8,
-    padding: '8px 0',
-    [theme.breakpoints.down('xs')]: {
-      gridTemplateColumns: 'auto',
-    },
-  },
-  form: {
-    [theme.breakpoints.down('xs')]: {
-      display: 'none',
-    },
-  },
-}));
+const AppWrapper = styled(Container)({
+  position: 'relative',
+  display: 'grid',
+  gridTemplateRows: 'auto 1fr auto',
+  minHeight: '100vh',
+  padding: 8,
+  borderRadius: 0,
+  background: '#4D4D4D',
+  fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+  zIndex: 0,
+});
+
+const MainContent = styled(CardContent)({
+  display: 'grid',
+  gridTemplateColumns: 'minmax(300px, 33%) auto',
+  gap: 8,
+  padding: '8px 0',
+});
 
 const App = () => {
-  const classes = useStyles();
+  const matches = useMediaQuery('(min-width:600px)');
 
   return (
-    <Paper className={classes.root}>
+    <AppWrapper maxWidth="md">
       <Header />
-      <CardContent className={classes.main}>
-        <div className={classes.form}>
+      <MainContent
+        style={{
+          gridTemplateColumns: matches ? 'minmax(300px, 33%) auto' : 'auto',
+        }}
+      >
+        <div
+          style={{
+            display: matches ? 'block' : 'none',
+          }}
+        >
           <CreateNoteForm />
         </div>
         <Board />
-      </CardContent>
+      </MainContent>
       <Footer />
-    </Paper>
+    </AppWrapper>
   );
 };
 
