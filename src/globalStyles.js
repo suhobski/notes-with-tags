9b88bdfa9f2c73f71ef88fd75f3ef12ec8bc1add
Alex <suhobski@mail.ru>
2021-10-31 15:24:17 +0300

    feat: add global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,48 @@
+const globalStyles = {
+  '*': {
+    boxSizing: 'border-box',
+  },
+  '*::before': {
+    boxSizing: 'border-box',
+  },
+  '*::after': {
+    boxSizing: 'border-box',
+  },
+  'ol[class]': {
+    padding: 0,
+    margin: 0,
+    listStyle: 'none',
+  },
+  body: {
+    margin: 0,
+    scrollBehavior: 'smooth',
+    textRendering: 'optimizeSpeed',
+    lineHeight: 1.5,
+  },
+  h1: {
+    margin: 0,
+  },
+  h2: {
+    margin: 0,
+  },
+  h3: {
+    margin: 0,
+  },
+  h4: {
+    margin: 0,
+  },
+  p: {
+    margin: 0,
+  },
+  li: {
+    margin: 0,
+  },
+  input: {
+    font: 'inherit',
+  },
+  button: {
+    font: 'inherit',
+  },
+};
+
+export default globalStyles;
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import App from './App';
 import rootReducer from './store/reducers/rootReducer';
+import globalStyles from './globalStyles';
 
 const store = createStore(rootReducer);
 
 const app = (
   <Provider store={store}>
+    <GlobalStyles styles={globalStyles} />
     <App />
   </Provider>
 );
